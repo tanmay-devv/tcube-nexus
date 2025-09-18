9d4c3eb11b170974445e33be1f8d41b5c4258c0d
tanmay-devv <tanmaysharma3141@gmail.com>
2025-09-18 11:02:13 +0530

    algo

--- testing/main.c
+++ testing/main.c
@@ -14,18 +14,49 @@ void drawBoard() {
 
 void turn(int array[3][3]) {
 	int row, col;
+	int counter = 0;
 	scanf("%d %d", &row, &col);
 
+	
+
 	if (row >= 0 && row < 3 && col >=0 && col < 3) {
+// for X		
+		if ( counter % 2 == 0 && counter ==! 0) {
+			printf("X's turn");
+			int array[row][col];
+			memset(array, 1, row*col*sizeof(int));
+			array[row][col] = 2;
+			counter += 1;
+			printf("%d\n",counter);	
+		} else if (counter == 0) {
+			// special case for 0
+			printf("O's turn");
+			int array[row][col];
+			memset(array, 1, row*col*sizeof(int));
+			array[row][col] = 1;
+			counter += 1;	
+			printf("%d\n",counter);	
+		} else {
+			// for O
+			printf("O's turn");
+			int array[row][col];
+			memset(array, 1, row*col*sizeof(int));
+			array[row][col] = 1;
+			counter += 1;	
+			printf("%d\n",counter);	
+		}
 		int array[row][col];
 		memset( array, 1, row*col*sizeof(int));
+		array[row][col] = 1;
+		counter += 1;
+
 		printf("%d",array[row][col]);
 
 		printf("successfully set! \n");
 	} else {
 		printf("please enter valid row and col\n");
 	}
-		
+	turn(array);		
 
 }
 
@@ -36,7 +67,7 @@ int main() {
 //			{0,0,0}};
 
 	int board[3][3];	
-	turn(board);	
+	turn(board);
 
 	return 0;
 }
